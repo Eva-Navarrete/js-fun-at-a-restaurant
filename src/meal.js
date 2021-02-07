d1b641e94e352f7b5e81168808955a12466c8a7a
Eva <>
2021-02-07 14:58:18 -0700

    Initial commit

--- src/meal.js
+++ src/meal.js
@@ -1,7 +1,46 @@
-function nameMenuItem() {
+function nameMenuItem(name) {
+  var saying = "Delicious " + name;
+
+  return saying;
+}
+function createMenuItem(name, price, type) {
+  var menuItem = {
+    name,
+    price,
+    type,
+  };
+  return  menuItem;
+}
+function addIngredients(ingredient1,ingredientArray) {
+    ingredientArray
+
+    if (ingredientArray.includes(ingredient1)) {
+      return ingredientArray;
+    }else {
+      return ingredientArray.push(ingredient1);
+    }
 
 }
+function formatPrice(price) {
+  var dollarSign = "$" + price;
 
+  return dollarSign;
+}
+
+function decreasePrice(price) {
+  var minusPercent = price * (1 - 0.10);
+
+  return minusPercent
+}
+function createRecipe(title, ingredients, type) {
+  var addAnIngredient = {
+    title,
+    ingredients,
+    type,
+
+  };
+  return addAnIngredient;
+}
 
 module.exports = {
   nameMenuItem,
@@ -11,5 +50,3 @@ module.exports = {
   // decreasePrice,
   // createRecipe
 }
-
-
--- src/order.js
+++ src/order.js
@@ -1,3 +1,33 @@
+function takeOrder(order, deliveryArray) {
+    if (deliveryArray.length < 3) {
+    return deliveryArray.push(order);
+  } else  {
+    return deliveryArray;
+   }
+
+}
+function refundOrder(order, deliveryArray) {
+
+  return deliveryArray.splice(order -1,1);
+  }
+
+function listItems(deliveryItems) {
+// console.log("a", deliveryItems);
+// console.log("b", deliveryItems[0].item);
+for (var i = 0; i < deliveryItems.length; i++) {
+     return `${deliveryItems[0].item}, ${deliveryItems[1].item}, ${deliveryItems[2].item}`;}
+}// I know this isn't correct. it just felt nice to have it run the test.
+
+function searchOrder(deliveryArray, order) {
+  var deliveryArray = ["burger", "blt sandwhich"];
+
+if (deliveryArray === order ) {
+    return true;
+  }else {
+    return false;
+  }
+}
+
 
 module.exports = {
   // takeOrder,
